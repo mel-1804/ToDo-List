57fabcba2152beb11572ce3f1fb5320a57357e73
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2024-08-03 18:10:33 +0000

    terminado el ToDo List

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const inputTodoList = document.querySelector("#exampleFormControlInput");
 let valueOfInput = ''
 
-inputTodoList.addEventListener("change", (e) => {
-    console.log(e.target.value);
+inputTodoList.addEventListener("change", (e)=>{
+    // console.log(e.target.value);
+    valueOfInput = e.target.value
 })
 
 const containerList = document.querySelector("ul")
 
-inputTodoList.addEventListener("keydown", (e)=> {
+const deleteInput = (e)=>{
+    e.target.remove()
+}
+
+inputTodoList.addEventListener("keydown", (e)=>{
+    // para no imprimir un Enter vacío, sin texto
+    if(e.target.value == '')return
+    
     if(e.keyCode ===13){
         const Li = document.createElement("Li")
-        Li.textContent = valueOfInput;
+        Li.addEventListener("click", deleteInput);
+        Li.textContent = e.target.value;
         containerList.appendChild(Li);
+        // para vaciar el input luego de agregar la tarea a la lista
+        e.target.value = ''
     }
 })
